fb82133c9e0f81abbd824d3636bec44da44e1415
Patrik Weibull <patrik.weibull@reguity.com>
2019-02-12 08:44:37 +0000

    bugfix

--- validators/Sweden/person.js
+++ validators/Sweden/person.js
@@ -2,7 +2,8 @@ module.exports = {
   country: 'se',
   type: 'person',
   isValidNinFn: nin => { // "Personnummer"
-    if (!(/^[0-9]{6}[\-\+]?[0-9]{4}?$/.test(nin) || /^[0-9]{8}[\-\+]?[0-9]{4}?$/.test(nin))) { return false; }
+    if (!(/^[0-9]{6}[\-\+]?[0-9]{4}?$/.test(nin) ||
+          /^[0-9]{8}[\-\+]?[0-9]{4}?$/.test(nin))) { return false; }
     nin = nin.replace(/\D/g,'');
     let cs;
     if (nin.length === 12) {
