ed4465e036f078d24a16148a90f4a74e0560a886
Patrik Weibull <patrik.weibull@reguity.com>
2019-06-24 06:37:40 +0000

    ?

--- validators/Sweden/organization.js
+++ validators/Sweden/organization.js
@@ -1,7 +1,7 @@
 module.exports = {
   country: 'se',
   type: 'organization',
-  isValidNinFn: nin => { // "Organisationsnummer"
+  isValidNinFn: nin => { // "Organisationsnummer" 
     if (!/^[0-9]{6}-?[0-9]{4}?$/.test(nin)) { return false; }
     nin = nin.replace(/\D/g,'');
     let sum = '2121212121'.split('')
