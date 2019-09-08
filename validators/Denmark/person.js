6d5279c3bec0aedeaf61739b614884bd7292e8da
Patrik Weibull <patrik.weibull@reguity.com>
2019-09-08 21:31:21 +0000

    Beautification

--- validators/Denmark/person.js
+++ validators/Denmark/person.js
@@ -8,7 +8,7 @@ module.exports = {
     let sum = '4327654321'.split('')
       .reduce((s,c,i) => s + nin[i] * c, 0);
     if (sum % 11 !== 0) { return false; }
-    let [ d, m, y ] = nin.split(/(.{2})/).filter(c => c).slice(0,3);
+    let [ d, m, y ] = nin.split(/(.{2})/).filter(c => c).slice(0, 3);
     return [ 19, 20 ].some(p => {
       let date = new Date(`${p}${y}-${m}-${d}`);
       return Boolean(+date) && date.getDate() == d;
